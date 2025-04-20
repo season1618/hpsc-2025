0ed63f6a1cf3864a3bbcff27463f40f84198731e
season1618 <seasonminority314@gmail.com>
2025-04-20 22:03:46 +0900

    02_openmp/14_bucket_sort: parallel for

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -3,12 +3,12 @@
 #include <vector>
 
 int main() {
-  int n = 50;
-  int range = 5;
+  int n = 50000000;
+  int range = 500;
   std::vector<int> key(n);
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
-    printf("%d ",key[i]);
+    //printf("%d ",key[i]);
   }
   printf("\n");
 
@@ -18,15 +18,17 @@ int main() {
   std::vector<int> offset(range,0);
   for (int i=1; i<range; i++) 
     offset[i] = offset[i-1] + bucket[i-1];
+#pragma omp parallel for
   for (int i=0; i<range; i++) {
-    int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
+    int base = offset[i];
+#pragma omp parallel for
+    for (int j=0; j<bucket[i]; j++) {
+      key[base+j] = i;
     }
   }
 
   for (int i=0; i<n; i++) {
-    printf("%d ",key[i]);
+    //printf("%d ",key[i]);
   }
   printf("\n");
 }
