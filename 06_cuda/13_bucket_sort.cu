0cae0b8cf43516742ef544a6cd0674751c45a797
season1618 <seasonminority314@gmail.com>
2025-05-04 20:17:38 +0900

    06_cuda/13_bucket_sort: run on gpu

--- 06_cuda/13_bucket_sort.cu
+++ 06_cuda/13_bucket_sort.cu
@@ -2,23 +2,31 @@
 #include <cstdlib>
 #include <vector>
 
+__global__ void thread(int *bucket, int *key) {
+    int i = threadIdx.x;
+    atomicAdd(&bucket[key[i]], 1);
+}
+
 int main() {
   int n = 50;
   int range = 5;
-  std::vector<int> key(n);
+
+  int *key, *bucket;
+  cudaMallocManaged(&bucket, range * sizeof(int));
+  cudaMallocManaged(&key, n * sizeof(int));
+  
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
     printf("%d ",key[i]);
   }
   printf("\n");
 
-  std::vector<int> bucket(range); 
   for (int i=0; i<range; i++) {
     bucket[i] = 0;
   }
-  for (int i=0; i<n; i++) {
-    bucket[key[i]]++;
-  }
+
+  thread<<<1, n>>>(bucket, key);
+  cudaDeviceSynchronize();
   for (int i=0, j=0; i<range; i++) {
     for (; bucket[i]>0; bucket[i]--) {
       key[j++] = i;
