ae78649446295409a43e5af68d63c6b9bcc69f66
season1618 <seasonminority314@gmail.com>
2025-06-08 20:23:24 +0900

    final: CUDA

--- 13_scientific/10_cavity_cuda.cu
+++ 13_scientific/10_cavity_cuda.cu
@@ -29,6 +29,8 @@ __global__ void init() {
   int id = blockIdx.x * blockDim.x + threadIdx.x;
   int j = id / nx;
   int i = id % nx;
+  if (j >= ny) return;
+
   u[j][i] = 0;
   v[j][i] = 0;
   p[j][i] = 0;
@@ -39,6 +41,7 @@ __global__ void compute_b() {
   int id = blockIdx.x * blockDim.x + threadIdx.x;
   int j = id / nx;
   int i = id % nx;
+  if (j >= ny) return;
   
   // Compute b[j][i]
   float dudx = (u[j][i+1] - u[j][i-1]) / (2 * dx);
@@ -50,34 +53,32 @@ __global__ void compute_b() {
 }   
 
 __global__ void copy_p() {
-  /*int id = blockIdx.x * blockDim.x + threadIdx.x;
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
   int j = id / nx;
   int i = id % nx;
-  */
-
-  for (int j=0; j<ny; j++)
-    for (int i=0; i<nx; i++)
-      pn[j][i] = p[j][i];
+  if (j >= ny) return;
+  
+  pn[j][i] = p[j][i];
 }
 
 __global__ void compute_p() {
-  /*int id = blockIdx.x * blockDim.x + threadIdx.x;
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
   int j = id / nx;
   int i = id % nx;
-  */
+  if (j >= ny) return;
 
-  //if (j == 0 || j == ny-1) return;
-  //if (i == 0 || i == nx-1) return;
+  float *a = &p[j][i];
+
+  if (j == 0) j = 1;
+  else if (j == ny-1) { *a = 0; return; }
+  else if (i == 0) i = 1;
+  else if (i == nx-1) i = nx-2;
   
-  for (int j=1; j<ny-1; j++) {
-    for (int i=1; i<nx-1; i++) {
-      // Compute p[j][i]
-      p[j][i] = (dy*dy * (pn[j][i+1] + pn[j][i-1]) +
-                 dx*dx * (pn[j+1][i] + pn[j-1][i]) -
-                 b[j][i] * dx*dx * dy*dy)
-                / (2 * (dx*dx + dy*dy));
-    }
-  }
+  // Compute p[j][i]
+  *a = (dy*dy * (pn[j][i+1] + pn[j][i-1]) +
+             dx*dx * (pn[j+1][i] + pn[j-1][i]) -
+             b[j][i] * dx*dx * dy*dy)
+           / (2 * (dx*dx + dy*dy));
 }
 
 __global__ void compute_p_rest() {      
@@ -97,6 +98,7 @@ __global__ void copy_uv() {
   int id = blockIdx.x * blockDim.x + threadIdx.x;
   int j = id / nx;
   int i = id % nx;
+  if (j >= ny) return;
 
   un[j][i] = u[j][i];
   vn[j][i] = v[j][i];
@@ -106,13 +108,25 @@ __global__ void compute_uv() {
   int id = blockIdx.x * blockDim.x + threadIdx.x;
   int j = id / nx;
   int i = id % nx;
+  if (j >= ny) return;
   
   // Compute u[j][i] and v[j][i]
-  //if (j == 0 || j == ny-1) return;
-  //if (i == 0 || i == nx-1) return;
+  if (j == 0) {
+    u[0][i] = 0;
+    v[0][i] = 0;
+    return;
+  }
+  if (j == ny-1) {
+    u[ny-1][i] = 1;
+    v[ny-1][i] = 0;
+    return;
+  }
+  if (i == 0 || i == nx-1) {
+    u[j][i] = 0;
+    v[j][i] = 0;
+    return;
+  }
   
-  for (int j=1; j<ny-1; j++) {
-      for (int i=1; i<nx-1; i++) {
   u[j][i] = un[j][i] - un[j][i] * dt / dx * (un[j][i] - un[j][i - 1])
                      - vn[j][i] * dt / dy * (un[j][i] - un[j - 1][i])
                      - dt / (2 * rho * dx) * (p[j][i+1] - p[j][i-1])
@@ -123,24 +137,6 @@ __global__ void compute_uv() {
                      - dt / (2 * rho * dx) * (p[j+1][i] - p[j-1][i])
                      + nu * dt / (dx*dx) * (vn[j][i+1] - 2 * vn[j][i] + vn[j][i-1])
                      + nu * dt / (dy*dy) * (vn[j+1][i] - 2 * vn[j][i] + vn[j-1][i]);
-      }}
-}
-
-__global__ void compute_uv_rest() {
-    for (int j=0; j<ny; j++) {
-      // Compute u[j][0], u[j][nx-1], v[j][0], v[j][nx-1]
-      u[j][0]  = 0;
-      u[j][nx-1] = 0;
-      v[j][0]  = 0;
-      v[j][nx-1] = 0;
-    }
-    for (int i=0; i<nx; i++) {
-      // Compute u[0][i], u[ny-1][i], v[0][i], v[ny-1][i]
-      u[0][i]  = 0;
-      u[ny-1][i] = 1;
-      v[0][i]  = 0;
-      v[ny-1][i] = 0;
-    }
 }
 
 float u_cpu[ny][nx];
@@ -161,24 +157,19 @@ int main() {
     compute_b<<<N_BLOCK, N_THREAD>>>();
     cudaDeviceSynchronize();
     for (int it=0; it<nit; it++) {
-      //copy_p<<<N_BLOCK, N_THREAD>>>();
-      copy_p<<<1, 1>>>();
+      copy_p<<<N_BLOCK, N_THREAD>>>();
       cudaDeviceSynchronize();
 
-      //compute_p<<<N_BLOCK, N_THREAD>>>();
-      compute_p<<<1, 1>>>();
+      compute_p<<<N_BLOCK, N_THREAD>>>();
       cudaDeviceSynchronize();
 
-      compute_p_rest<<<1, 1>>>();
-      cudaDeviceSynchronize();
+      //compute_p_rest<<<1, 1>>>();
+      //cudaDeviceSynchronize();
     }
     copy_uv<<<N_BLOCK, N_THREAD>>>();
     cudaDeviceSynchronize();
 
-    compute_uv<<<1, 1>>>();
-    cudaDeviceSynchronize();
-
-    compute_uv_rest<<<1, 1>>>();
+    compute_uv<<<N_BLOCK, N_THREAD>>>();
     cudaDeviceSynchronize();
 
     if (n % 10 == 0) {
@@ -204,3 +195,5 @@ int main() {
   vfile.close();
   pfile.close();
 }
+
+
