f820b454afc059c30b127cd6ade1b2f663332c1b
season1618 <seasonminority314@gmail.com>
2025-06-08 16:28:27 +0900

    final: C++

--- 13_scientific/10_cavity.cpp
+++ 13_scientific/10_cavity.cpp
@@ -39,6 +39,10 @@ int main() {
     for (int j=1; j<ny-1; j++) {
       for (int i=1; i<nx-1; i++) {
         // Compute b[j][i]
+	b[j][i] = rho * (1 / dt *
+                  ((u[j][i+1] - u[j][i-1]) / (2 * dx) + (v[j+1][i] - v[j-1][i]) / (2 * dy)) -
+                  ((u[j][i+1] - u[j][i-1]) / (2 * dx))*((u[j][i+1] - u[j][i-1]) / (2 * dx)) - 2 * ((u[j+1][i] - u[j-1][i]) / (2 * dy) *
+                   (v[j][i+1] - v[j][i-1]) / (2 * dx)) - ((v[j+1][i] - v[j-1][i]) / (2 * dy))*((v[j+1][i] - v[j-1][i]) / (2 * dy)));
       }
     }
     for (int it=0; it<nit; it++) {
@@ -48,13 +52,21 @@ int main() {
       for (int j=1; j<ny-1; j++) {
         for (int i=1; i<nx-1; i++) {
 	  // Compute p[j][i]
+	  p[j][i] = (dy*dy * (pn[j][i+1] + pn[j][i-1]) +
+                     dx*dx * (pn[j+1][i] + pn[j-1][i]) -
+                     b[j][i] * dx*dx * dy*dy)
+                    / (2 * (dx*dx + dy*dy));
 	}
       }
       for (int j=0; j<ny; j++) {
         // Compute p[j][0] and p[j][nx-1]
+	p[j][nx-1] = p[j][nx-2];
+        p[j][0] = p[j][1];
       }
       for (int i=0; i<nx; i++) {
 	// Compute p[0][i] and p[ny-1][i]
+	p[0][i] = p[1][i];
+	p[ny-1][i] = 0;
       }
     }
     for (int j=0; j<ny; j++) {
@@ -66,13 +78,31 @@ int main() {
     for (int j=1; j<ny-1; j++) {
       for (int i=1; i<nx-1; i++) {
 	// Compute u[j][i] and v[j][i]
+	u[j][i] = un[j][i] - un[j][i] * dt / dx * (un[j][i] - un[j][i - 1])
+                           - vn[j][i] * dt / dy * (un[j][i] - un[j - 1][i])
+                           - dt / (2 * rho * dx) * (p[j][i+1] - p[j][i-1])
+                           + nu * dt / (dx*dx) * (un[j][i+1] - 2 * un[j][i] + un[j][i-1])
+                           + nu * dt / (dy*dy) * (un[j+1][i] - 2 * un[j][i] + un[j-1][i]);
+        v[j][i] = vn[j][i] - un[j][i] * dt / dx * (vn[j][i] - vn[j][i - 1])
+                           - vn[j][i] * dt / dy * (vn[j][i] - vn[j - 1][i])
+                           - dt / (2 * rho * dx) * (p[j+1][i] - p[j-1][i])
+                           + nu * dt / (dx*dx) * (vn[j][i+1] - 2 * vn[j][i] + vn[j][i-1])
+                           + nu * dt / (dy*dy) * (vn[j+1][i] - 2 * vn[j][i] + vn[j-1][i]);
       }
     }
     for (int j=0; j<ny; j++) {
       // Compute u[j][0], u[j][nx-1], v[j][0], v[j][nx-1]
+      u[j][0]  = 0;
+      u[j][nx-1] = 0;
+      v[j][0]  = 0;
+      v[j][nx-1] = 0;
     }
     for (int i=0; i<nx; i++) {
       // Compute u[0][i], u[ny-1][i], v[0][i], v[ny-1][i]
+      u[0][i]  = 0;
+      u[ny-1][i] = 1;
+      v[0][i]  = 0;
+      v[ny-1][i] = 0;
     }
     if (n % 10 == 0) {
       for (int j=0; j<ny; j++)
